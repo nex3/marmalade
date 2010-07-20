cb8d295b93b42ef2d70f80f3642483d2532eaa40
Nathan Weizenbaum <nweiz@google.com>
2010-07-20 14:16:18 -0700

    Add a basic sexp parser.

---
+++ parser.js
@@ -0,0 +1,57 @@
+exports.parseSexp = function(str) {
+    var parser = new Parser(str);
+    return parser.exp();
+};
+
+
+var Parser = function(str) {
+    this._str = str;
+};
+
+Parser.prototype.exp = function() {
+    return this._sexp() || this._symbol() || this._number();
+};
+
+var sys = require("sys");
+
+Parser.prototype._tok = function(rx) {
+    var match = this._str.match(new RegExp("^" + rx.source));
+    if (!match) return false;
+    this._str = this._str.substring(match[0].length);
+    return match;
+};
+
+Parser.prototype._assert = function(rx) {
+    var tok = this._tok(rx);
+    if (tok) return tok;
+    throw "Syntax error: expected " + rx + ", was " + this._str;
+};
+
+Parser.prototype._ws = function() {
+    while (this._tok(/\s+/) || this._tok(/;[^\n]*/));
+};
+
+Parser.prototype._sexp = function() {
+    if (!this._tok(/\(/)) return false;
+    this._ws();
+    var val = [];
+    var exp;
+    while (exp = this.exp()) {
+        val.push(exp);
+        this._ws();
+    }
+    this._assert(/\)/);
+    return val;
+};
+
+Parser.prototype._symbol = function() {
+    var tok = this._tok(/([^0-9.#"'()\[\]\\`\s]|\\.)([^#"'()\[\]\\`\s]|\\.)*/);
+    if (!tok) return false;
+    else return tok[0];
+};
+
+Parser.prototype._number = function() {
+    var tok = this._tok(/[0-9]*(\.[0-9]+)/);
+    if (!tok) return false;
+    else return +tok[0];
+};
