879c5216b4edd8a372c41a6ddd32b810a3b330cf
Nathan Weizenbaum <nweiz@google.com>
2010-07-22 17:53:53 -0700

    Add a simple server using express.js.

---
+++ server.js
@@ -0,0 +1,24 @@
+var connect = require("connect"),
+    express = require("express");
+
+exports.create = function(middleware) {
+    var app = express.createServer(
+        connect.logger(),
+        connect.gzip(),
+        connect.errorHandler(),
+        connect.conditionalGet()
+    );
+
+    app.addListener('listening', function() {
+        var address = app.address();
+        var hostname = address.address;
+        if (hostname === "0.0.0.0") hostname = "localhost";
+        console.log("Jelly's spread all over " + hostname + ":" + address.port);
+    });
+
+    app.get('/', function(req, res) {
+        res.send("<h1>Jelly - Elisp Packages on Toast</h1>");
+    });
+
+    return app;
+};
