92aa0eab07e8a1d46348f7857cba2bed65f9b045
Nathan Weizenbaum <nweiz@google.com>
2010-07-22 19:55:06 -0700

    Add a backend function for reading package info from our files.

--- backend.js
+++ backend.js
@@ -10,3 +10,27 @@ exports.savePackage = function(elisp, callback) {
         });
     });
 };
+
+exports.getPackages = function(callback) {
+    fs.readdir('packages', function(err, files) {
+        if (err) return callback(err, []);
+
+        var packages = [];
+        var errors = [];
+        var filesToParse = 0;
+        for (var i = 0; i < files.length; i++) {
+            var file = 'packages/' + files[i];
+            filesToParse += 1;
+            fs.readFile(file, "utf8", function(err, data) {
+                if (err) {
+                    console.log("Error reading " + file + ": " + err);
+                } else {
+                    packages.push(packageParser.parse(data));
+                }
+
+                filesToParse -= 1;
+                if (filesToParse === 0) callback(null, packages);
+            });
+        }
+    });
+};
