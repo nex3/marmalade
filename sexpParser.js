c8d59a91f0af9f1c496d87da169914bc0acd9e1c
Nathan Weizenbaum <nweiz@google.com>
2010-08-09 16:47:10 -0700

    Document sexpParser.

--- sexpParser.js
+++ sexpParser.js
@@ -1,26 +1,57 @@
+/**
+ * An error class raised when parsing elisp fails.
+ * @param {string} msg The error message.
+ * @constructor
+ */
 var SyntaxError = exports.SyntaxError = util.errorClass('SyntaxError');
 
+/**
+ * Parse an Elisp s-expression. Lists become arrays, symbols and strings become
+ * strings, numbers become numbers.
+ * @param {string} str The Elisp code to parse.
+ * @return {*} The JS value of the Elisp expression.
+ */
 exports.parse = function(str) {
     var parser = new Parser(str);
     return parser.exp();
 };
 
 
+/**
+ * A class that maintains the parser state.
+ * @param {string} str The string to parse.
+ * @constructor
+ */
 var Parser = function(str) {
     this._str = str;
 };
 
+/**
+ * Parses a single Elisp expression.
+ * @return {*} The JS value of the Elisp expression.
+ */
 Parser.prototype.exp = function() {
     return this._sexp() || this._symbol() || this._number() || this._string();
 };
 
+/**
+ * Consumes a token and moves the parser forward.
+ * @param {RegExp} rx The token to consume.
+ * @return {?Array} The match data for the token, or null if it wasn't matched.
+ */
 Parser.prototype._tok = function(rx) {
     var match = this._str.match(new RegExp("^" + rx.source));
-    if (!match) return false;
+    if (!match) return null;
     this._str = this._str.substring(match[0].length);
     return match;
 };
 
+/**
+ * Consumes a token and moves the parser forward. If the token does not exist,
+ * throws an error.
+ * @param {RegExp} rx The token to consume.
+ * @return {Array} The match data for the token.
+ */
 Parser.prototype._assert = function(rx) {
     var tok = this._tok(rx);
     if (tok) return tok;
@@ -28,12 +59,19 @@ Parser.prototype._assert = function(rx) {
                           this._str);
 };
 
+/**
+ * Consumes whitespace and comments.
+ */
 Parser.prototype._ws = function() {
     while (this._tok(/\s+/) || this._tok(/;[^\n]*/));
 };
 
+/**
+ * Parses a parenthesized list.
+ * @return {?Array} The parsed list, or null if it wasn't matched.
+ */
 Parser.prototype._sexp = function() {
-    if (!this._tok(/\(/)) return false;
+    if (!this._tok(/\(/)) return null;
     this._ws();
     var val = [];
     var exp;
@@ -45,22 +83,34 @@ Parser.prototype._sexp = function() {
     return val;
 };
 
+/**
+ * Parses a symbol.
+ * @return {?string} The symbol value, or null if it wasn't matched.
+ */
 Parser.prototype._symbol = function() {
     var tok = this._tok(/([^0-9.#"'()\[\]\\`\s]|\\.)([^#"'()\[\]\\`\s]|\\.)*/);
-    if (!tok) return false;
+    if (!tok) return null;
     var sym = tok[0].replace(/\\(.)/g, '$1');
     sym.lispType = 'symbol';
     return sym;
 };
 
+/**
+ * Parses a number.
+ * @return {?number} The number value, or null if it wasn't matched.
+ */
 Parser.prototype._number = function() {
     var tok = this._tok(/[0-9]*(\.[0-9]+)/);
-    if (!tok) return false;
+    if (!tok) return null;
     else return +tok[0];
 };
 
+/**
+ * Parses a string.
+ * @return {?string} The string value, or null if it wasn't matched.
+ */
 Parser.prototype._string = function() {
     var tok = this._tok(/"((?:[^"]|\\.)*)"/);
-    if (!tok) return false;
+    if (!tok) return null;
     else return tok[1].replace(/\\[ \n]/g, "").replace(/\\(.)/g, '$1');
 };
