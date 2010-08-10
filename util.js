8405d3776934e2cb27841f03c1a2e0f0dde16857
Nathan Weizenbaum <nweiz@google.com>
2010-08-09 17:15:55 -0700

    Document util.

--- util.js
+++ util.js
@@ -4,6 +4,15 @@ var spawn = require("child_process").spawn,
     step = require("step"),
     _ = require("underscore")._;
 
+/**
+ * Run an executable, optionally with a chunk of text to pass to stdin, and call
+ * the callback with the output.
+ * @param {String} command The name of the executable to run.
+ * @param {Array.<string>} args The arguments to the executable.
+ * @param {string=} input The string to pass via stdin.
+ * @param {function(Error=, string)} callback Passed the collected standard
+ *   output from the process.
+ */
 exports.run = function(command, args, input, callback) {
     if (callback === undefined) {
         callback = input;
@@ -15,11 +24,14 @@ exports.run = function(command, args, input, callback) {
     var stdout = [];
     var stderr = [];
     child.on('error', callback);
-    child.stdout.on('data', function(data) {stdout.push(data.toString('utf8'))});
-    child.stderr.on('data', function(data) {stderr.push(data.toString('utf8'))});
+    child.stdout.on('data',
+                    function(data) {stdout.push(data.toString('utf8'))});
+    child.stderr.on('data',
+                    function(data) {stderr.push(data.toString('utf8'))});
     child.on('exit', function(code, signal) {
         if (code !== 0) {
-            var err = new Error("Process " + command + " terminated unexpectedly.");
+            var err = new Error("Process " + command +
+                                " terminated unexpectedly.");
             err.code = code;
             err.signal = signal;
             err.command = command;
@@ -36,6 +48,16 @@ exports.run = function(command, args, input, callback) {
     if (input) child.stdin.end(input, 'utf8');
 };
 
+/**
+ * Creates a class inheriting from `Error`. This class's constructor will take
+ * at least one parameter, the error message.
+ *
+ * @param {string|function} nameOrFn If a string, the name of the class to
+ *   create. If a function, a named function that is used as (part of) the
+ *   constructor for the new class. The message parameter is not passed to the
+ *   function, but other parameters are.
+ * @return {function} The class constructor.
+ */
 exports.errorClass = function(nameOrFn) {
     var name = nameOrFn;
     var fn;
