a2d1e409d70e604ce8e004b6b68c833439c3d465
Nathan Weizenbaum <nweiz@google.com>
2010-09-01 13:44:56 -0700

    Styling using Sass and Compass.

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "lib/sass"
+css_dir = "lib/public/stylesheets"
+http_css_dir = "stylesheets"
+images_dir = "lib/public/images"
+http_images_dir = "images"
+javascripts_dir = "lib/public/javascripts"
+http_javascripts_dir = "javascripts"
+environment = :production
+sass_options = {:style => :compressed}
